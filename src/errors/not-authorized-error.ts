a25d6939f8075fd02838aa37f4c18d79300879f1
Chloe-Ren94 <chloeren316@gmail.com>
2024-07-26 21:23:27 -0700

    updates

---
+++ src/errors/not-authorized-error.ts
@@ -0,0 +1,14 @@
+import { CustomError } from "./custom-error";
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 404;
+  
+  constructor() {
+    super('Not authorized');
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+
+  serializaErrors() {
+    return [{ message: 'Not authorized' }];
+  }
+}
